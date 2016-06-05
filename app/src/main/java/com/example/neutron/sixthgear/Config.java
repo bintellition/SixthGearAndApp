719e2444175d4b4255ef17232eeae3c58c258a4f
bintellition <bintellition@outlook.com>
2016-06-05 18:33:08 +0100

    Initial commit

---
+++ app/src/main/java/com/example/neutron/sixthgear/Config.java
@@ -0,0 +1,11 @@
+package com.example.neutron.sixthgear;
+
+/**
+ * Created by NeutroN on 22/05/2016.
+ */
+public class Config {
+    public static final String FILE_UPLOAD_URL = "http://192.168.0.6/AndroidFileUpload/fileUpload.php";
+
+    // Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
+}
